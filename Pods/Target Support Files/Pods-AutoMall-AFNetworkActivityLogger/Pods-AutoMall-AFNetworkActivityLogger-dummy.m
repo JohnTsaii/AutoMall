cfabe3050561b9e43c231e4cc15a752b1a4c579c
shenxf_myMacAir <446200660@qq.com>
2015-09-25 21:01:32 +0800

    dele pod

--- Pods/Target Support Files/Pods-AutoMall-AFNetworkActivityLogger/Pods-AutoMall-AFNetworkActivityLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AutoMall_AFNetworkActivityLogger : NSObject
-@end
-@implementation PodsDummy_Pods_AutoMall_AFNetworkActivityLogger
-@end
--- Pods/Target Support Files/Pods-AutoMall-MJRefresh/Pods-AutoMall-MJRefresh-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AutoMall_MJRefresh : NSObject
-@end
-@implementation PodsDummy_Pods_AutoMall_MJRefresh
-@end

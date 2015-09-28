4c8b8f12b3c2492c8ae3473e8346de9888fd948d
John Tsai <caijingzhuang@gmail.com>
2015-09-28 20:54:18 +0800

    add tabbar item image

--- AutoMall/ViewControllers/MainViewController/MainViewController.swift
+++ AutoMall/ViewControllers/MainViewController/MainViewController.swift
@@ -62,15 +62,30 @@ class MainViewController: UITabBarController {
         
         self.setTabbarItemAppearance()
         
+        
         self.view.backgroundColor = UIColor.whiteColor()
     }
     
     private func setTabbarItemAppearance() {
         self.homeVC.title = "首页"
+        self.homeNav.tabBarItem.image = UIImage(named: "home_item_unselected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.homeNav.tabBarItem.selectedImage = UIImage(named: "home_item_selected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        
         self.sortVC.title = "分类"
+        self.sortNav.tabBarItem.image = UIImage(named: "sort_item_unselected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.sortNav.tabBarItem.selectedImage = UIImage(named: "sort_item_selected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        
         self.searchVC.title = "筛选"
+        self.searchNav.tabBarItem.image = UIImage(named: "search_item_unselected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.searchNav.tabBarItem.selectedImage = UIImage(named: "search_item_selected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        
         self.shopCarVC.title = "购物车"
+        self.shopCarNav.tabBarItem.image = UIImage(named: "shop_car_item_unselected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.shopCarNav.tabBarItem.selectedImage = UIImage(named: "shop_car_item_selected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        
         self.mineVC.title = "我的"
+        self.mineNav.tabBarItem.image = UIImage(named: "mine_item_unselected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.mineNav.tabBarItem.selectedImage = UIImage(named: "mine_item_selected")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
     }
 
     override func didReceiveMemoryWarning() {
